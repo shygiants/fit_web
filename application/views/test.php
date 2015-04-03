a582cca03c0bbb9c994c799169459680ab42ec92
shygiants <shygiants@nate.com>
2015-04-04 02:34:32 +0900

    Added Materialize Added grapic controller Added test page

---
+++ application/views/test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset='utf-8'/>
+			<!--Import materialize.css-->
+			<link type="text/css" rel="stylesheet" href="<?=base_url('library/materialize/css/materialize.min.css')?>"  media="screen,projection"/>
+			<!--Let browser know website is optimized for mobile-->
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+		</head>
+		<body>
+			<div class="container">
+				<h1>Hello world!</h1>
+
+
+				<!--Import jQuery before materialize.js-->
+				<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+				<script type="text/javascript" src="<?=base_url('library/materialize/js/materialize.min.js')?>"></script>
+			</div>
+		</body>
+	</html>
