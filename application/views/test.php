57f03b86362a9d4ad2f352fa362d5aa21a7c52da
shygiants <shygiants@nate.com>
2015-04-05 16:21:32 +0900

    Added .htaccess for security

--- application/views/test.php
+++ application/views/test.php
@@ -8,7 +8,29 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 		</head>
 		<body>
+			<nav>
+				<div class="nav-wrapper">
+					<a href="#" class="brand-logo center">fit</a>
+					<ul id="nav-mobile" class="right hide-on-med-and-down">
+						<li><a class="modal-trigger" href="#modal1">Sign in</a></li>
+					</ul>
+				</div>
+			</nav>
+
+			<!-- Modal Structure -->
+			<div id="modal1" class="modal">
+						<div class="modal-content">
+							<h4>Modal Header</h4>
+							<p>A bunch of text</p>
+						</div>
+						<div class="modal-footer">
+							<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+						</div>
+					</div>
+
+
 			<div class="container">
+
 				<h1>Hello world!</h1>
 
 
